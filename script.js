f5d55f249731442904866ed4a482295a9f180571
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-22 18:48:52 +0100

    pozivanje metode za racunanje brzine trcanja u okviru klase

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ class Running extends Workout {
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
+    this.calcPace();
   }
 
   calcPace() {
