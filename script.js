ee0c2df6999250ab781403578f0c5133b62a443a
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-24 20:39:58 +0100

    prikaz markera na mapi iz local storage

--- script.js
+++ script.js
@@ -116,6 +116,10 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
