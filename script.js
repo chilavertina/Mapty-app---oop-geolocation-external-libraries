a83c055de975db769621918b3bbb6f52ed18a2b7
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-24 13:24:00 +0100

    podesavanje opisa iznad markera

--- script.js
+++ script.js
@@ -198,7 +198,9 @@ class App {
           className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('workout')
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`
+      )
       .openPopup();
   }
   _renderWorkout(workout) {
